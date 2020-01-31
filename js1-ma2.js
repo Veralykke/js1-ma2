8cb2f1fdde962570d3d3e266cdb4790845d5ca08
Vera Lykke Kaasen <55652971+Veralykke@users.noreply.github.com>
2020-01-31 13:59:44 +0100

    Create js1-ma2.js

---
+++ js1-ma2.js
@@ -0,0 +1,160 @@
+//QUESTION: 1
+
+//const myFunctionExpression = function() {
+//console.log("Vera Lykke Kaasen");
+//}
+
+//myFunctionExpression();
+
+
+
+
+
+//QUESTION: 2 
+
+//const button = document.querySelector(".btn");
+
+//button.addEventListener("click", callAfterButtonClick);
+
+//function callAfterButtonClick() {
+//console.log("I was clicked");
+//};
+
+
+
+
+
+//Question: 3 
+
+
+
+//document.querySelector("#firstName").addEventListener("keydown", callAfterButtonClick2);
+
+//function callAfterButtonClick2() {
+//console.log("value:", event.target.value);
+//};
+
+
+
+
+//QUESTION: 4 
+
+//const button = document.querySelectorAll("button")[1];
+
+//button.addEventListener("mouseover", callOnHover);
+
+//function callOnHover() {
+//console.log("hover");
+//}
+
+
+//QUESTION: 5 
+
+//const button = document.querySelector("[data-animal]");
+
+//button.addEventListener("mouseout", onHoverRemove);
+
+//function onHoverRemove() {
+//onHoverRemove.classList.remove("Hover");
+//};
+
+
+
+
+//QUESTION: 6 ???
+
+/*const allItems = document.querySelectorAll("li");
+
+
+for (let i = 0; i < allItems.length; i++) {
+    allItems[i].addEventListener("mouseover", animalHover);
+
+
+    function animalHover(allItems) {
+        console.log(allItems);
+    };*/
+
+
+
+let listItems = document.querySelectorAll("li");
+listItems.forEach(function(item) {
+    item.addEventListener("mouseover", listAllValues)
+});
+
+function listAllValues(e) {
+    console.log(e.target.dataset.animal);
+}
+
+
+
+//QUESTION: 7 
+
+//const animal = "cat";
+
+
+//switch (animal) {
+
+//case "cat":
+//console.log("Meow");
+//break;
+
+//case "cow":
+//console.log("Moo");
+//break;
+
+//case "bird":
+//console.log("Tweet");
+//break;
+
+//default:
+//console.log("Harruph");
+//}
+
+
+
+
+//Question: 8 
+
+//const sheep = ["Malcolm", "Anders", "Marie"];
+
+//sheep.forEach(function(sheep) {
+//console.log(sheep);
+//});
+
+
+
+
+//QUESTION: 9 
+
+
+//function logTime() {
+//console.log("hello");
+
+//if (counter === 6) {
+
+//clearInterval(intervalId);
+//}
+
+//counter++
+
+//}
+
+//let counter = 1;
+
+
+//const intervalId = setInterval(logTime, 500);
+
+
+
+
+
+
+//QUESTION: 10
+
+//const timeoutContainer = document.querySelector("div");
+
+//function updateDiv() {
+//timeoutContainer.innerHTML = "Text updated"
+//}
+
+//setTimeout(updateDiv, 2000);
